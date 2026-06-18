19230971bb899215969c34f1043cde0af2f619d7
HoangBao <vohoangbaoqn123@gmail.com>
2026-06-18 19:11:53 +0700

    add

---
+++ src/main/java/com/example/mxhconnectify/dto/SearchUserDTO.java
@@ -0,0 +1,15 @@
+package com.example.mxhconnectify.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SearchUserDTO {
+    private String username;
+    private String fullName;
+    private String avatarUrl;
+}
